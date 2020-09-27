9f46dcccc956f632fbcd65e10628af6e25fa3202
JaminXie <389098898@qq.com>
2020-09-28 00:36:51 +0800

    Github CI 测试 6

--- deploy-gitee.sh
+++ deploy-gitee.sh
@@ -10,9 +10,16 @@ cd docs/.vuepress/dist
 # deploy to gitee
 msg='deploy'
 # 设置gitee的用户名
-git config --global user.name "JaminXie"
-git config --global user.email "389098898@qq.com"
-githeeUrl=https://gitee.com/JaminXie/JaminXie.git  # gitee项目地址
+if [ -z "$GITEE_TOKEN" ]; then   # 如果有GITEE_TOKEN环境变量，说明是在github Ci上执行，这个环境变量在github上对于的仓库设置隐私变量
+  msg='deploy'
+  githeeUrl=https://gitee.com/JaminXie/JaminXie.git  # gitee项目地址
+else
+  msg='来自github actions的自动部署'
+  githeeUrl=https://JaminXie:${GITEE_TOKEN}@gitee.com/JaminXie/JaminXie.git  # 使用github CI
+  git config --global user.name "JaminXie"
+  git config --global user.email "389098898@qq.com"
+fi
+
 git init
 git add -A
 git commit -m "${msg}"
@@ -21,6 +28,7 @@ git push -f $githeeUrl master # 推送到gitee
 cd - # 退回开始所在目录
 rm -rf docs/.vuepress/dist
 
-# 依赖puppeteer
-# 可以用 cnpm install puppeteer 安装
-node deploy-gitee.js
+if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
+  # 依赖puppeteer
+  # 可以用 cnpm install puppeteer 安装
+  node deploy-gitee.js

dbbc98c2884a8e7477b0638d649d08c8bf8ac239
JaminXie <389098898@qq.com>
2020-09-27 22:00:33 +0800

    注释掉看板娘，有的小笔记本体验不好

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -101,34 +101,34 @@ module.exports = {
       excludeClassName: 'theme-vdoing-content' // 要排除元素的class, 默认空''
     }],
 
-      // 看板娘
-          [
-      'vuepress-plugin-helper-live2d', {
-        // 是否开启控制台日志打印(default: false)
-        log: false,
-        live2d: {
-          // 是否启用(关闭请设置为false)(default: true)
-          enable: true,
-          // 模型名称(default: hibiki)>>>取值请参考：
-          // https://github.com/JoeyBling/hexo-theme-yilia-plus/wiki/live2d%E6%A8%A1%E5%9E%8B%E5%8C%85%E5%B1%95%E7%A4%BA
-          model: 'shizuku',
-          // model: 'tororo',
-          display: {
-            position: "left", // 显示位置：left/right(default: 'right')
-            width: 205, // 模型的长度(default: 135)
-            height: 450, // 模型的高度(default: 300)
-            hOffset: 45, //  水平偏移(default: 65)
-            vOffset: -30, //  垂直偏移(default: 0)
-          },
-          mobile: {
-            show: false // 是否在移动设备上显示(default: false)
-          },
-          react: {
-            opacity: 1 // 模型透明度(default: 0.8)
-          }
-        }
-      }
-    ],
+    //   // 看板娘
+    //       [
+    //   'vuepress-plugin-helper-live2d', {
+    //     // 是否开启控制台日志打印(default: false)
+    //     log: false,
+    //     live2d: {
+    //       // 是否启用(关闭请设置为false)(default: true)
+    //       enable: true,
+    //       // 模型名称(default: hibiki)>>>取值请参考：
+    //       // https://github.com/JoeyBling/hexo-theme-yilia-plus/wiki/live2d%E6%A8%A1%E5%9E%8B%E5%8C%85%E5%B1%95%E7%A4%BA
+    //       model: 'shizuku',
+    //       // model: 'tororo',
+    //       display: {
+    //         position: "left", // 显示位置：left/right(default: 'right')
+    //         width: 205, // 模型的长度(default: 135)
+    //         height: 450, // 模型的高度(default: 300)
+    //         hOffset: 45, //  水平偏移(default: 65)
+    //         vOffset: -30, //  垂直偏移(default: 0)
+    //       },
+    //       mobile: {
+    //         show: false // 是否在移动设备上显示(default: false)
+    //       },
+    //       react: {
+    //         opacity: 1 // 模型透明度(default: 0.8)
+    //       }
+    //     }
+    //   }
+    // ],
 
 
     ['thirdparty-search', { // 可以添加第三方搜索链接的搜索框（原官方搜索框的参数仍可用）

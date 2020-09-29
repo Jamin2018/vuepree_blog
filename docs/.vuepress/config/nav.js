ba0fb001eae283ee7463c59832c5981a9a033f8c
JaminXie <389098898@qq.com>
2020-09-30 00:02:49 +0800

    新增算法目录

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -30,6 +30,7 @@ module.exports = [
     text: '技术',
     link: '/technology/',
     items: [
+      {text: '数据结构与算法', link: '/pages/859e87/'},
       {text: '技术文档', link: '/pages/9a7ee40fc232253e/'},
       {text: 'GitHub技巧', link: '/pages/4c778760be26d8b3/'},
       // {text: 'Nodejs', link: '/pages/117708e0af7f0bd9/'},
